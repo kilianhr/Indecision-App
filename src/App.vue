bf9e51e0c2b92fd46df9de76c0233791b3f5980d
Kilian Henares <khenares@topdoctors.es>
2023-08-25 15:21:38 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'App',
+  components: {
+    Indecision: import('./components/Indecision.vue')
+  }
+}
+</script>
+
+<template>
+  <Indecision />
+</template>
+
+<style>
+</style>
